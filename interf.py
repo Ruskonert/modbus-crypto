59378e4912ac0928044079da19e81a4ed11ab7eb
Ruskonert <ruskonert@gmail.com>
2019-09-19 23:42:38 +0900

    Initial commit

---
+++ interf.py
@@ -0,0 +1,98 @@
+import socket
+import os
+import modbus_socket
+import time
+import struct
+
+host = '10.211.55.3'
+port = 502
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect((host, port))
+
+client = modbus_socket.ModbusClient(sock)
+print('Connected server=[{}:{}]'.format(host, port))
+
+def print_header():
+    os.system('clear')
+    print()
+    print()
+    print('\t'*2 + 'Raw-based Modbus Client (github.com/ruskonert)')
+
+while True:
+    print_header()
+    print('\t'*2 + 'Choose the function you want to do:')
+    print('\t' + '='*60)
+    print('\t'*2 + '[1] Set Function => [{}]'.format(client.function_code.data))
+    if client._reference is None:
+        raddress = 'Undefined'
+    else:
+        raddress = struct.unpack('>H', client._reference)[0]
+
+    if client._count is None:
+        count = 'Undefined'
+    else:
+        count = struct.unpack('>H', client._count)[0]
+
+    print('\t'*2 + '[2] Set reference address => [{}]'.format(raddress))
+    print('\t'*2 + '[3] Set r/w count => [{}]'.format(count))
+    print('\t'*2 + '[4] Exploit')
+    print('\t'*2 + '[5] Set data offset [not implemented]')
+    print('\t'*2 + '[6] Close socket')
+    print('\t' + '='*60)
+    number = input('\t'*2 + "Choose your method (default: 4): ")
+
+    if number == '':
+        number = 4
+    else:
+        number = int(number)
+
+    if number == 1:
+        print_header()
+        print('\t' + '='*60)
+        print('\t'*2 + 'Read Coli                = 0x01')
+        print('\t'*2 + 'Read Input Register      = 0x04')
+        print('\t'*2 + 'Read Holding Register    = 0x03')
+        print('\t'*2 + 'Read Discrete Inputs     = 0x02')
+        print('\t'*2 + 'Write Single Coil        = 0x05')
+        print('\t'*2 + 'Write Multiple Coils     = 0x0F')
+        print('\t'*2 + 'Write Single Register    = 0x06')
+        print('\t'*2 + 'Write Multiple Registers = 0x10')
+        print('\t' + '='*60)
+        func = int(input('\t'*2 + 'Which do you want? => '), 16)
+        client.apply_function(func)
+    elif number == 2:
+        ref_number = input('\t'*2 + 'Which do you want? (default: 0) => ')
+        if ref_number == '':
+            ref_number = 0
+        else:
+            ref_number = int(ref_number)
+        client.set_slave_id(ref_number)
+    elif number == 3:
+        rw = input('\t'*2 + 'Which do you want? (default: 1) => ')
+        if rw == '':
+            rw = 1
+        else:
+            rw = int(rw)
+        client.set_rw_count(rw)
+    elif number == 4:
+        print('\t'*2 + "Function => [{}]".format(hex(client.function_code.data)))
+        print('\t'*2 + "Exploit  => " + str(bytes(client.get_modbus_header())))
+        client.send()
+        recv_data = bytes(sock.recv(1024))
+        print('\t'*2 + "Result   => " + str(recv_data))
+
+        
+        if recv_data[-1] == 0x03:
+            print('\t'*2 + "Exception unexpected: Illegal data value")
+        elif recv_data[-1] == 0x01:
+            print('\t'*2 + "Exception unexpected: Illegal function code")
+        else:
+            print('\t'*2 + "Successful.")
+        
+        print('\t'*2 + 'Please any key continue ...')
+        input()
+    elif number == 6:
+        sock.close()
+        print("goodbye")
+        break
---
+++ modbus_socket.py
@@ -0,0 +1,139 @@
+import struct
+import socket
+import os
+import sys
+import random
+
+class Function:
+    class Read:
+        Coli = 0x01
+        Input_Register = 0x04
+        Holding_Register = 0x03
+        Discrete_Inputs= 0x02
+    class Write:
+        Single_Coil = 0x05
+        Multiple_Coils = 0x0F
+        Single_Register = 0x06
+        Multiple_Registers = 0x10
+
+class PacketElement:
+    def __init__(self, data, length):
+        self.data = data
+        self.length = length
+
+    @staticmethod
+    def get_string_pack(data):
+        if isinstance(data, int):
+            if data > 0 and data <= 0xFFFF:
+                return 'H'
+            elif data > 0xFFFF:
+                return 'I'
+            else:
+                return 'B'
+        else:
+            return 's'
+
+    @staticmethod
+    def get_length_pack(length):
+        if length == 1:
+            return 'B'
+        if length == 2:
+            return 'H'
+        if length == 4:
+            return 'I'
+        else:
+            return 'L'
+    
+    def create(self):
+        if isinstance(self.data, int):
+            return struct.pack('{}{}'.format('>', PacketElement.get_length_pack(self.length)), self.data)
+        elif isinstance(self.data, str):
+            return self.data.encode('utf-8')
+
+class ModbusClient:
+    def __init__(self, socket_base):
+        self.transaction_id = PacketElement(0x01, 2)
+
+        # The modbus protocol id, It always constantly 0x00 on TCP
+        self.protocol_id = PacketElement(0x00, 2)
+
+        # TCP Port always 0x01
+        self.unit_id = PacketElement(0x01, 1)
+
+        # The function code, The default is Read Coli (0x01)
+        self.function_code = PacketElement(0x01, 1)
+        self._socket_base = socket_base
+
+        self._reference = None
+        self._count = None
+
+        self._data_count = 0
+
+    def apply_function(self, fcode):
+        self.function_code = PacketElement(fcode, 1)
+
+    # reference Number (Slave id)
+    def set_slave_id(self, id):
+        self._reference = bytearray(struct.pack('>H', id))
+
+    def set_rw_count(self, count):
+        self._count = bytearray(struct.pack('>H', count))
+        self._data_count = count
+
+    def get_modbus_header(self):
+        # Generate packet header, but others need to calcuate length.
+        tdata = bytearray(self.transaction_id.create())
+        tdata += bytearray(self.protocol_id.create())
+
+        # unit id
+        unit_id = bytearray(self.unit_id.create())
+
+        # function code
+        function_code = bytearray(self.function_code.create())
+        
+        # reference Number (Slave id)
+        reference = self._reference
+        
+        # data (Bit count, it means how many read to bits?)
+        data = self._count
+
+        multi_write_mode = False
+
+        if self.function_code.data == 0x0f:
+            multi_write_mode = True
+            byte_count = bytearray(struct.pack('>B', 2))
+            register_data = bytearray(bytes.fromhex(hex(random.randrange(0, 0xffff)).replace('0x', '') * 2))
+
+        elif self.function_code.data == 0x10:
+            multi_write_mode = True
+            # it needs to write some data.
+            byte_count = bytearray(struct.pack('>B', self._data_count*2))
+            # randomness value, 2 bytes equal 1 register value (there is 120 register value)
+            register_data = bytearray(bytes.fromhex('1234567890abcdefaabbccddeeff1a1b1c1d1e1f'*12))[0:self._data_count*4]
+            # Combines data without header.
+        
+        if multi_write_mode:
+            pdata = unit_id + function_code + reference + data + byte_count + register_data
+        else:
+            # Combines data without header.
+            pdata = unit_id + function_code + reference + data
+
+        # Calucates the length.
+        length = bytearray(PacketElement(len(pdata), 2).create())
+
+        # Append length (2 bytes)
+        tdata += length
+
+        # Append packet data
+        tdata += pdata
+        return tdata
+
+    def _next_trans_id(self):
+        if self.transaction_id.data > 0xFFFF:
+            self.transaction_id.data = 0
+        else:
+            self.transaction_id.data = self.transaction_id.data + 1
+          
+    def send(self):
+        self._socket_base.send(self.get_modbus_header())
+        self._next_trans_id()
